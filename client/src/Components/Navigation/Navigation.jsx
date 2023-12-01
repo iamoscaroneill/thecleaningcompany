1cd6651cee1d5f27f569ea8c0179305324ca43c5
Oscar O'Neill <theoscaroneill@aol.com>
2023-12-01 04:00:54 -0500

    initialized frontend

---
+++ client/src/Components/Navigation/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import nav from './Navigation.module.css'
+
+const Navigation = () => {
+  return (
+    <div className={nav.container}>
+        The Cleaning Company
+    </div>
+  )
+}
+
+export default Navigation
